bcebad5077a9ad20eed41c737727856a1b952bff
Miklos Csepella <csepella.miki@gmail.com>
2016-05-17 12:12:15 +0200

    abc

---
+++ hundred_doors.py
@@ -0,0 +1,16 @@
+"""x = 0
+list = []
+while x < 100:
+    list[x] = list.append(0)
+    x = x + 1"""
+list = [0] * 100
+for a in range(1, 101):
+    for b in range(100):
+        if (b + 1) % a == 0:
+            if list[b] == 0:
+                list[b] = 1
+            else:
+                list[b] = 0
+for c in range(100):
+    if list[c] == 1:
+        print(c + 1, ". ", sep="")
